8fe910c384b8e13ddabc2fa234d7279816f9c2c4
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2024-09-02 14:36:04 +0100

    added correct api link

--- src/hooks/apiHooks.ts
+++ src/hooks/apiHooks.ts
@@ -19,7 +19,7 @@ export interface MenuItem {
 }
 
 // API base URL
-const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Adjust to your Laravel server
+const API_BASE_URL = 'https://menu-management-backend.onrender.com/api'; // Adjust to your Laravel server
 
 // Hook to get all menus
 export const useMenus = () => {
